f3a2ada2e2dd068f8686c87da114f3fc13bb52f6
Ed1lan <edi.youtub@gmail.com>
2022-04-30 18:03:41 +0200

    Enter

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function abrirweb() {
+    var button_audio = new Audio('sounds/minecraft_click.mp3');
+    button_audio.play();
+    var delayMilis = 100;
+        setTimeout(function() {
+            window.location.replace("web/dudaweb.htm");
+        }, delayMilis);
+}
+function volverweb() {
+    var button_audio = new Audio('../sounds/minecraft_click.mp3');
+    button_audio.play();
+    var delayMilis = 100;
+        setTimeout(function() {
+            window.location.replace("../index.htm");
+        }, delayMilis);
+}
+
+function fotosintesis() {
+    window.location.replace("https://www.youtube.com/watch?v=xJxr1piIfSU");
+}
