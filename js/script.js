7c83967b54dce022afc9b8c7f625eed14c741679
Ed1lan <edi.youtub@gmail.com>
2022-05-01 00:16:27 +0200

    Enter

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-function abrirweb() {
+function abrirweb(x) {
     var button_audio = new Audio('sounds/minecraft_click.mp3');
     button_audio.play();
     var delayMilis = 100;
+    var username = x.value;
+    var compareuser = username.toUpperCase();
+    if(compareuser == "HEROBRINE") {
+        window.location.replace("https://www.youtube.com/watch?v=xvFZjo5PgG0")
+    } else {
         setTimeout(function() {
             window.location.replace("web/dudaweb.htm");
         }, delayMilis);
+    }
+    
 }
 function volverweb() {
     var button_audio = new Audio('../sounds/minecraft_click.mp3');
@@ -17,4 +24,19 @@ function volverweb() {
 
 function fotosintesis() {
     window.open("https://www.youtube.com/watch?v=xJxr1piIfSU");
+}
+
+function abrirPanel(x) {
+    if(x.id == "pag-mods") {
+        alternarPaneles();
+    } else if(x.id == "pag-eventos") {
+        alternarPaneles();
+    } else if(x.id == "pag-normas") {
+        alternarPaneles();
+    }
+}
+
+function alternarPaneles() {
+    document.getElementById("barra-lateral").style.display = "flex";
+    document.getElementById("container-pags-selec-id").style.display = "none";
 }
